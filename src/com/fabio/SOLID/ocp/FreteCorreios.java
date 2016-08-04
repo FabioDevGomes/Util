9532fbc99801d3c2fd4d49fca2ab58bc8e688af7
Fábio <fabio.dev.gomes@gmail.com>
2016-08-04 14:26:34 -0300

    Boas práticas basedas nos princípios SOLID / OCP
    Atualização sem IDE

--- src/com/fabio/SOLID/ocp/FreteCorreios.java
+++ src/com/fabio/SOLID/ocp/FreteCorreios.java
@@ -1,8 +1,9 @@
 package com.fabio.SOLID.ocp;
 
 
-public class FreteCorreios {
+public class FreteCorreios implements ServicoDeEntrega{
 
+  @Override
   public double calcularFrete(String cidade){
     if("SAO PAULO".equals(cidade.toUpperCase())){
       return 15;
